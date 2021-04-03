ab69810c5a22082e8f8802b78c0e86225580a4ab
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-03 23:15:30 +0530

    feat(board): gen board from game state

--- static/js/counter.js
+++ static/js/counter.js
@@ -2,6 +2,42 @@ const addBtn = document.querySelector('#add');
 const subBtn = document.querySelector('#sub');
 const resetBtn = document.querySelector('#reset');
 const val = document.querySelector('#count');
+const board = document.querySelector('#board');
+let data = [...Array(8)].map(() =>
+  Array(8).fill({
+    player: Math.floor(Math.random() * 2) + 1,
+    value: Math.floor(Math.random() * 2),
+  })
+);
+
+console.log(data);
+
+board.addEventListener('click', (e) => {
+  if (e.target.className === 'cell') {
+    const id = e.target.id;
+    document.querySelector(`#${id}`).style.backgroundColor = 'red';
+  }
+});
+
+data.forEach((r, i) => {
+  const row = document.createElement('div');
+  row.classList.add(`row`);
+  row.id = `r${i}`;
+  r.forEach((c, j) => {
+    const cell = document.createElement('div');
+    cell.classList.add(`cell`);
+    cell.id = `c${i}${j}`;
+    if (c.value !== null) {
+      if (c.player === 2) {
+        cell.style.backgroundColor = 'red';
+      } else {
+        cell.style.backgroundColor = 'blue';
+      }
+    }
+    row.appendChild(cell);
+  });
+  board.appendChild(row);
+});
 
 let counter = 0;
 let initVal = 0;
